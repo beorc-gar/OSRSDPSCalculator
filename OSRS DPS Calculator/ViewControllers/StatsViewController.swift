12e4ca5ee11033a6aabcf42019fa0f1c7044d162
ᛒᚸ <9055837+beorc-gar@users.noreply.github.com>
2019-02-14 14:48:24 -0500

    Update StatsViewController.swift

--- OSRS DPS Calculator/ViewControllers/StatsViewController.swift
+++ OSRS DPS Calculator/ViewControllers/StatsViewController.swift
@@ -2,9 +2,6 @@
 //  SecondViewController.swift
 //  OSRS DPS Calculator
 //
-//  Created by Bronson Graansma on 2018-03-09.
-//  Copyright © 2018 Bronson Graansma. All rights reserved.
-//
 
 import UIKit
 
