d672cf80fbf14a9e815a741d12018a996cea8718
ᛒᚸ <9055837+beorc-gar@users.noreply.github.com>
2019-02-14 14:49:06 -0500

    Update TabBarControllerWrapper.swift

--- OSRS DPS Calculator/TabBarControllerWrapper.swift
+++ OSRS DPS Calculator/TabBarControllerWrapper.swift
@@ -2,9 +2,6 @@
 //  TabBarControllerWrapper.swift
 //  OSRS DPS Calculator
 //
-//  Created by Bronson Graansma on 2018-03-09.
-//  Copyright © 2018 Bronson Graansma. All rights reserved.
-//
 
 import Foundation
 import UIKit
